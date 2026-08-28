0ba5e3f6da6996b2e445dae5937ff70ab3da656f
DISHANT SAINI <143501891+DishantSaini55@users.noreply.github.com>
2026-08-28 22:19:49 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int l=0;
+        int r=1;
+        int maxprofit=0;
+
+        while(r<n){
+            if(prices[l]<prices[r]){
+                maxprofit=max(maxprofit,prices[r]-prices[l]);
+            }
+            else{
+                l=r;
+            }
+            r++;
+        }
+
+        return maxprofit;
+
+    }
+};
