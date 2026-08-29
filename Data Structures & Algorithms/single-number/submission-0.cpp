1fbbd4b4c3ac075c7ffddf91e5de3eb982cb51f9
DISHANT SAINI <143501891+DishantSaini55@users.noreply.github.com>
2026-08-29 21:46:39 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        map<int,int>mpp;
+        
+        for(auto it:nums){
+            mpp[it]++;
+        }
+
+        for(auto it:mpp){
+            if(it.second==1){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
