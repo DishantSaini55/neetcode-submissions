c1ef6461cdf363972e95c6c742cb8377e25e87d5
DISHANT SAINI <143501891+DishantSaini55@users.noreply.github.com>
2026-09-07 01:52:14 +0530

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int f(int i,vector<int>& cost,vector<int>&dp){
+        if(i==0){
+            return 0;
+        }
+        if(i==1){
+            return 0;
+        }
+
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        return dp[i]= min(f(i-1,cost,dp)+cost[i-1],f(i-2,cost,dp)+cost[i-2]) ;
+    }
+    int minCostClimbingStairs(vector<int>& cost) {  
+        vector<int>dp(cost.size()+1,-1);
+        return f(cost.size(),cost,dp);
+        
+    }
+};
