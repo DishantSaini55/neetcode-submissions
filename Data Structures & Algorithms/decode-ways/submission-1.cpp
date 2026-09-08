bd59990a3af8445cd53810c4633bfb1e0ad5f8f8
DISHANT SAINI <143501891+DishantSaini55@users.noreply.github.com>
2026-09-08 21:48:04 +0530

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    int f(int i,string &s,vector<int>&dp){
+        if(i==s.size()){
+            return 1;
+        }
+
+        if(s[i]=='0'){
+            return 0;
+        }
+
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+
+        int one=f(i+1,s,dp);
+
+        int two=0;
+
+        if(i+1<s.size()){
+            int num=(s[i]-'0')*10+(s[i+1]-'0');
+
+            if(num>=10 && num<=26){
+                two=f(i+2,s,dp);
+            }
+        }
+
+        return dp[i]=one+two;
+
+    }
+    int numDecodings(string s) {
+        vector<int>dp(s.size()+1,-1);
+        return f(0,s,dp);
+    }
+};
